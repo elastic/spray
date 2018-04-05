25efe20460a99d1658d328ddf7f0d7fc7513aa2b
Sylvain Wallez <sylvain@elastic.co>
2018-04-05 18:17:44 +0200

    Add a "clear" method on the HttpRequestRedactor object

--- spray-http/src/main/scala/spray/http/HttpRequestRedactor.scala
+++ spray-http/src/main/scala/spray/http/HttpRequestRedactor.scala
@@ -5,6 +5,10 @@ trait HttpRequestRedactor {
   def redact(req: HttpRequest): HttpRequest
 }
 
+/**
+  * A global set of request redactors. This mutable set allows every component of an application to register
+  * redactors that filter out what's relevant to them.
+  */
 object HttpRequestRedactor {
   private var redactors = Set.empty[HttpRequestRedactor]
 
@@ -14,5 +18,10 @@ object HttpRequestRedactor {
 
   def redact(request: HttpRequest): HttpRequest = {
     redactors.foldLeft(request)((req, redactor) ⇒ redactor.redact(req))
+  }
+
+  /** Clears the redactors that have been registered. Use with care, meant to be used in tests. */
+  def clear(): Unit = this.synchronized {
+    redactors = Set.empty
   }
 }
